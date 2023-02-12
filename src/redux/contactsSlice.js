26f6ffac4fa1c2b364a35d670938d751e5340f9e
UlaşDK <udk97@hotmail.com>
2023-02-12 14:17:24 +0300

    react redux-toolkit

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const contactsSlice = createSlice({
+    name : "contacts",
+    initialState:{
+        items:[]
+    },
+    reducers:{}
+})
+
+export default contactsSlice.reducer
